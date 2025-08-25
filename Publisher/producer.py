b18000c727d7d538bb2b9a01353c6dd694a6287e
Daniel Fisher <danielg16fisher@gmail.com>
2025-08-25 19:40:19 +0300

    Badic producer class

--- Publisher/producer.py
+++ Publisher/producer.py
@@ -4,4 +4,15 @@ from sklearn.datasets import fetch_20newsgroups
 import json
 
 class Producer:
-    pass
+    def __init__(self, broker='localhost:9092'):
+        self.producer = KafkaProducer(bootstrap=broker)
+
+    def send_message(self, topic, message):
+        if isinstance(message, str):
+            message = message.encode('utf-8')
+
+        self.producer.send(topic, message)
+        self.producer.flush()
+
+    def close(self):
+        self.producer.close()
--- Subscriber/consumer.py
+++ Subscriber/consumer.py
@@ -1,5 +1,4 @@
 from kafka import KafkaConsumer, KafkaProducer
 import json
 
-class Consumer:
-    pass
+
